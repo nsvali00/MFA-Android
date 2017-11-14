384ee7bfff73f1f5722e5ff6e9031ddf60038d9d
Nikola Svalina <nikolasvalina001@gmail.com>
2017-11-14 23:44:31 +0100

    Add after login activity

--- app/src/main/java/com/example/mfa/mfa_android/MainActivity.java
+++ app/src/main/java/com/example/mfa/mfa_android/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.mfa.mfa_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText Email = (EditText)findViewById(R.id.email);
+        final EditText Password = (EditText)findViewById(R.id.password);
+        Button Login = (Button)findViewById(R.id.btnLogin);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            public void onClick(View view) {
+                validate(Email.getText().toString(), Password.getText().toString());
+            }
+        });
+    }
+
+    private void validate(String userEmail, String userPassword) {
+        if ((userEmail.contains("@")) && (userPassword.length() > 4)) {
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+        }
     }
 }
